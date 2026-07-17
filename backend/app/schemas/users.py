e2b759120b8fd934bb70601502cd75e1a2947513
akashway <akashmishraway@gmail.com>
2026-07-18 00:43:57 +0530

    Created users schema

---
+++ backend/app/schemas/users.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from typing import Literal
+from app.models.users import UserRole
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=128)
+    full_name: str = Field(min_length=2, max_length=100)
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    full_name: str
+    role: UserRole
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: Literal["bearer"] = "bearer"
+
+class TokenData(BaseModel):
+    user_id : int | None = None
