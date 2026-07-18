1908f33629b15ee1430911f8e268f9a6d21f4200
akashway <akashmishraway@gmail.com>
2026-07-18 19:53:26 +0530

    Create schema of airport

---
+++ backend/app/schemas/airport.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, ConfigDict, Field, field_validator
+
+
+class AirportCreate(BaseModel):
+
+    iata_code: str = Field(min_length=3, max_length=3)
+
+    @field_validator("iata_code")
+    @classmethod
+    def uppercase_iata(cls, value: str) -> str:
+        return value.upper()
+
+    name: str = Field(min_length=2, max_length=255)
+    city: str = Field(min_length=2, max_length=100)
+    country: str = Field(min_length=2, max_length=100)
+
+
+class AirportUpdate(BaseModel):
+    iata_code: str | None = Field(default=None, min_length=3, max_length=3)
+    name: str | None = Field(default=None, min_length=2, max_length=255)
+    city: str | None = Field(default=None, min_length=2, max_length=100,)
+    country: str | None = Field(default=None, min_length=2, max_length=100)
+
+
+class AirportResponse(BaseModel):
+    id: int
+    iata_code: str
+    name: str
+    city: str
+    country: str
+
+    model_config = ConfigDict(from_attributes=True)
