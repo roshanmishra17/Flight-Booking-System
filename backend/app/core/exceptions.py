8e0b56a1524d0e7d841209b5cbb395dd288a2263
akashway <akashmishraway@gmail.com>
2026-07-18 20:12:43 +0530

    Add the exceptions for airport

--- backend/app/core/exceptions.py
+++ backend/app/core/exceptions.py
@@ -1,3 +1,5 @@
+# Authentication Exceptions
+
 class EmailAlreadyExistsError(Exception):
     """Raised when a user tries to register with an existing email."""
     pass
@@ -5,4 +7,18 @@ class EmailAlreadyExistsError(Exception):
 
 class InvalidCredentialsError(Exception):
     """Raised when login credentials are invalid."""
+    pass
+
+class AirportAlreadyExistsError(Exception):
+    """Raised when an airport with the given IATA code already exists."""
+    pass
+
+
+#Airport Exceptions
+class AirportNotFoundError(Exception):
+    """Raised when the requested airport does not exist."""
+    pass
+
+class AirportInUseError(Exception):
+    """Raised when an airport cannot be deleted because flights reference it."""
     pass
