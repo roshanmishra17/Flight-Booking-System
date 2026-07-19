48fede593d67bb318d8853c61f4ca2023d0cb22e
akashway <akashmishraway@gmail.com>
2026-07-20 00:25:54 +0530

    Add the flight router in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.auth import router as auth_router
 from app.api.airport import router as airport_router
+from app.api.flights import router as flight_router
 app = FastAPI()
 
 # @app.get('/')
@@ -9,3 +10,4 @@ app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(airport_router)
+app.include_router(flight_router)
