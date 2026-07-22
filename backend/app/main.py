9a1ed2fc523d5c48269b9cc900164bb02971646c
akashway <akashmishraway@gmail.com>
2026-07-23 01:29:55 +0530

    Add the seat API in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.auth import router as auth_router
 from app.api.airport import router as airport_router
 from app.api.flights import router as flight_router
+from app.api.seat import router as seat_router
 app = FastAPI()
 
 # @app.get('/')
@@ -11,3 +12,4 @@ app = FastAPI()
 app.include_router(auth_router)
 app.include_router(airport_router)
 app.include_router(flight_router)
+app.include_router(seat_router)
