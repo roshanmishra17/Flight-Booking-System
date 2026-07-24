86254f0f1d4fc44eca9c8183b48f68ec756d71d2
akashway <akashmishraway@gmail.com>
2026-07-24 19:29:00 +0530

    Add the booking api

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,8 @@ from app.api.auth import router as auth_router
 from app.api.airport import router as airport_router
 from app.api.flights import router as flight_router
 from app.api.seat import router as seat_router
+from app.api.booking import router as booking_router
+
 app = FastAPI()
 
 # @app.get('/')
@@ -13,3 +15,4 @@ app.include_router(auth_router)
 app.include_router(airport_router)
 app.include_router(flight_router)
 app.include_router(seat_router)
+app.include_router(booking_router)
