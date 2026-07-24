67c5975ec8cb9f8941bbc8e33eb7bc96588b2b50
akashway <akashmishraway@gmail.com>
2026-07-25 01:26:52 +0530

    Add the payment api in main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from app.api.airport import router as airport_router
 from app.api.flights import router as flight_router
 from app.api.seat import router as seat_router
 from app.api.booking import router as booking_router
+from app.api.payment import router as payment_router
 
 app = FastAPI()
 
@@ -16,3 +17,4 @@ app.include_router(airport_router)
 app.include_router(flight_router)
 app.include_router(seat_router)
 app.include_router(booking_router)
+app.include_router(payment_router)
