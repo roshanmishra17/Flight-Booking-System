d74da7aee5c95fe0170140b515d8ba034d35c5ba
akashway <akashmishraway@gmail.com>
2026-08-02 01:51:55 +0530

    Fixed the config for password

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -17,7 +17,7 @@ print(Base.metadata.tables.keys())
 # this is the Alembic Config object, which provides
 # access to the es within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
+config.set_main_option("sqlalchemy.url", DATABASE_URL.replace("%", "%%"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
