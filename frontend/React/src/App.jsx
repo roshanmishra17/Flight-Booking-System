ce988d5a5dd3b8725c75823e2df448cdb9f0c73c
akashway <akashmishraway@gmail.com>
2026-08-04 00:55:33 +0530

    Fix the unwanted code

--- frontend/React/src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-    return <AppRoutes />;
-}
-
-export default App
--- frontend/React/src/api/userApi.jsx
+++ frontend/React/src/api/userApi.jsx
@@ -9,36 +9,3 @@ export async function getCurrentUser() {
     return response.data;
 
 }
-api.interceptors.request.use((config) => {
-
-    const token = localStorage.getItem("token");
-
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-
-});
-
-api.interceptors.response.use(
-
-    (response) => response,
-
-    (error) => {
-
-        if (error.response?.status === 401) {
-
-            localStorage.removeItem("token");
-
-            window.location.href = "/login";
-
-        }
-
-        return Promise.reject(error);
-
-    }
-
-);
-
-export default api;
