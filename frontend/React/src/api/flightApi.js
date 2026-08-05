f576b89de8604e3a4d4b87b7824e0f1d9a0a05b1
akashway <akashmishraway@gmail.com>
2026-08-06 01:09:50 +0530

    Add the api to fetch the flight

---
+++ frontend/React/src/api/flightApi.js
@@ -0,0 +1,28 @@
+import api from "./axios";
+
+export async function searchFlights({ origin, destination, departureDate }) {
+    const response = await api.get("/flights/search", {
+        params: { origin, destination, departure_date: departureDate },
+    });
+    return response.data;
+}
+
+export async function searchFlightsRanked({ origin, destination, departureDate, travelClass, mode }) {
+    const response = await api.get("/flights/search/ranked", {
+        params: {
+            origin,
+            destination,
+            departure_date: departureDate,
+            travel_class: travelClass,
+            mode,
+        },
+    });
+    return response.data;
+}
+
+export async function searchAlternativeRoutes({ origin, destination, departureDate }) {
+    const response = await api.get("/flights/search/alternatives", {
+        params: { origin, destination, departure_date: departureDate },
+    });
+    return response.data;
+}
