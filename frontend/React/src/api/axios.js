92891e961760dde1857ffada6917a7bcfaf9d354
akashway <akashmishraway@gmail.com>
2026-08-05 18:31:17 +0530

    Fix the access token

--- frontend/React/src/api/axios.js
+++ frontend/React/src/api/axios.js
@@ -24,9 +24,7 @@ api.interceptors.response.use(
         if (error.response?.status === 401) {
 
             localStorage.removeItem("token");
-
-            window.location.href = "/login";
-
+            
         }
 
         return Promise.reject(error);
