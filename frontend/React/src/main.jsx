1d93baefdd24d18e0adc05b2c6d51a867da03d73
akashway <akashmishraway@gmail.com>
2026-08-05 18:31:56 +0530

    Fix the imports

--- frontend/React/src/main.jsx
+++ frontend/React/src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext'
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from "./App";
+
 
 
 createRoot(document.getElementById('root')).render(
