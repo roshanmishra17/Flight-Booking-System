4dec6785a542f9478d9d80eb6671d570a53e7461
akashway <akashmishraway@gmail.com>
2026-08-10 18:41:29 +0530

    Add the getMyBookings and cancel Booking api

--- frontend/React/src/api/bookingApi.js
+++ frontend/React/src/api/bookingApi.js
@@ -8,3 +8,13 @@ export async function createBooking(data) {
     const response = await api.post("/bookings", data);
     return response.data;
 }
+
+export async function getMyBookings() {
+    const response = await api.get("/bookings/me");
+    return response.data;
+}
+
+export async function cancelBooking(bookingId) {
+    const response = await api.patch(`/bookings/${bookingId}/cancel`);
+    return response.data;
+}
