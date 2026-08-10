02014a47fe0ea1a67b3021d719a2ccbfcb3704a9
akashway <akashmishraway@gmail.com>
2026-08-10 20:35:24 +0530

    URL of frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:4173",
-        "https://flight-booking-system-nu-sand.vercel.app",
+        "skyroute-flight-booking-system.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
